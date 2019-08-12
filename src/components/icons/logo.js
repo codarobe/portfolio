d2ce6ddad3fa3afca35f3903d34815bde437f675
Cody Robertson <codarobe@ut.utm.edu>
2019-08-12 18:08:59 -0500

    Add custom logo for site and add accessibliity role setting for github and linkedin logos

---
+++ src/components/icons/logo.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const IconLogo = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 13.8 13.8">
+        <name>Cody Robertson Initials Logo</name>
+        <rect width="13.8" height="13.8" x="68.9" y="76.2" fill="#00005c" ry="2.4" transform="translate(-69 -76.2)"/>
+        <g fill="#00ff7a" stroke-width=".3" font-family="Arial" font-size="10.6" letter-spacing="0" transform="translate(-69 -76.2)" word-spacing="0">
+            <text style="line-height:1.25;-inkscape-font-specification:'Arial, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-feature-settings:normal;text-align:start" x="69" y="84.4">
+                <tspan x="69" y="84.4" style="-inkscape-font-specification:'Arial, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-feature-settings:normal;text-align:start">C</tspan>
+            </text>
+            <text style="line-height:1.25;-inkscape-font-specification:'Arial, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-feature-settings:normal;text-align:start" x="74.3" y="89">
+                <tspan x="74.3" y="89" style="-inkscape-font-specification:'Arial, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-feature-settings:normal;text-align:start">R</tspan>
+            </text>
+        </g>
+    </svg>
+);
+
+export default IconLogo;
