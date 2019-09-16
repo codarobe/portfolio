a4165e1392daaf233f54d31fb573877a8a19d0ab
Cody Robertson <codarobe@ut.utm.edu>
2019-09-16 10:41:54 -0500

    Add styling via styled-components

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,20 +1,33 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { media, Section, Button } from '@styles'
+//import PropTypes from 'prop-types';
+
+const HeroContainer = styled(Section)`
+  flex-direction: column;
+  align-items: flex-start;
+  min-height: 100vh;
+  ${media.tablet`padding-top: 150px;`};
+  div {
+    width: 100%;
+  }
+`;
 
 const Hero = ({ data }) => {
     const { frontmatter, html } = data[0].node;
     const { lead, name, subtitle, contactText } = frontmatter;
     return (
-        <div>
-            <h1>{lead}</h1>
-            <h2>{name}</h2>
-            <h3>{subtitle}</h3>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-            <button class="contactButton">{contactText}</button>
-        </div>
+        <HeroContainer>
+          <h2>{lead}</h2>
+          <h1>{name}</h1>
+          <h3>{subtitle}</h3>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <Button>{ contactText}</Button>
+        </HeroContainer>
     );
 }
 
+/*
 Hero.propTypes = {
     data: PropTypes.shape({
         html: PropTypes.string.isRequired,
@@ -26,5 +39,6 @@ Hero.propTypes = {
         }),
     }),
 };
+*/
 
 export default Hero;
