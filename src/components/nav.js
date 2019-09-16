4b955b2b21c0659688f781ecc1aae4e6e9ce86b9
Cody Robertson <codarobe@ut.utm.edu>
2019-09-16 10:41:28 -0500

    Update navbar with additional styling, display logo, and fix link to homepage

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,36 +1,44 @@
-import { Link } from "gatsby"
-import IconLogo from "./icons/logo"
-import PropTypes from "prop-types"
-import React from "react"
+import { Link } from "gatsby";
+import IconLogo from "./icons/logo";
+import PropTypes from "prop-types";
+import React from "react";
+import { theme, Button } from "@styles";
 
 const Nav = ({ siteTitle }) => (
   <nav
     style={{
-      background: "#02003a",
+      background: theme.colors.background,
       marginBottom: "1.45rem",
     }}
   >
-    <div style={{height: "42px", width: "42px"}}>
-      <IconLogo />
-    </div>
+
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        maxWidth: "85%",
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
+      <div style={{
+        height: "42px",
+        width: "42px",
+        float: "left"
+      }}>
         <Link
           to="/"
           style={{
-            color: `white`,
             textDecoration: `none`,
           }}
+          aria-label="Home"
         >
-          {siteTitle}
+          <IconLogo />
         </Link>
-      </h1>
+      </div>
+      <div style={{
+        float: "right"
+      }}>
+        <Button>Résumé</Button>
+      </div>
     </div>
   </nav>
 )
