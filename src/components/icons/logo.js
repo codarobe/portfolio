11464c81f8bddf9ab89de1243b7216e384be9373
Cody Robertson <codarobe@ut.utm.edu>
2019-09-16 10:29:10 -0500

    Update logo colors and fix compatibility with JSX

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -3,8 +3,8 @@ import React from "react";
 const IconLogo = () => (
     <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 13.8 13.8">
         <name>Cody Robertson Initials Logo</name>
-        <rect width="13.8" height="13.8" x="68.9" y="76.2" fill="#02003a" ry="2.4" transform="translate(-69 -76.2)"/>
-        <g fill="#00ff7a" stroke-width=".3" font-family="Arial" font-size="10.6" letter-spacing="0" transform="translate(-69 -76.2)" word-spacing="0">
+        <rect width="13.8" height="13.8" x="68.9" y="76.2" fill="#09071d" ry="2.4" transform="translate(-69 -76.2)"/>
+        <g fill="#00ff7a" strokeWidth=".3" fontFamily="Arial" fontSize="10.6" letterSpacing="0" transform="translate(-69 -76.2)" wordSpacing="0">
             <text style={{ lineHeight:1.25,fontVariantLigatures:"normal",fontVariantCaps:"normal",fontVariantNumeric:"normal",fontFeatureSettings:"normal",textAlign:"start"}} x="69" y="84.4">
                 <tspan x="69" y="84.4" style={{fontVariantLigatures:"normal",fFontVariantCaps:"normal",fontVariantNumeric:"normal",fontFeatureSettings:"normal",textAlign: "start"}}>C</tspan>
             </text>
